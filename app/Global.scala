bc9097b2d5e965acc86b7cc71d68612d8a55f260
ojacquemart <o.jacquemart@gmail.com>
2014-09-08 21:25:15 +0200

    feat: add play-metrics
    Basic app monitor, relative to #1

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,10 @@
+import com.kenshoo.play.metrics.MetricsFilter
+
 import core.CorsFilter
+
 import play.api._
 import play.api.mvc._
 
-object Global extends WithFilters(CorsFilter) with GlobalSettings {
+object Global extends WithFilters(CorsFilter, MetricsFilter) with GlobalSettings {
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,4 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.4")
 
 addSbtPlugin("de.johoop" % "jacoco4sbt" % "2.1.6")
+
